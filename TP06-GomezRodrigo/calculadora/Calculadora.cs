7cfe0d32cdc13a713fa8566b81319fb446666302
Roderick <iroderick@outlook.es>
2022-06-05 14:09:56 -0300

    calculadora completa

---
+++ TP06-GomezRodrigo/calculadora/Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    internal class Calculadora
+    {
+        public double resultado;
+        public void Sumar (double numero)
+        {
+            resultado += numero;
+        }
+        public void Restar (double numero)
+        {
+            resultado -= numero; 
+        }
+        public void Multiplicar (double numero)
+        {
+            resultado *= numero;
+        }
+        public void Dividir (double numero)
+        {
+            if (numero != 0)
+            {
+                resultado /= numero;
+            }
+            else
+            {
+                Console.WriteLine("No está definida la división por cero.");
+            }
+        }
+        public void Clear()
+        {
+            resultado = 0;
+        }
+        public Calculadora (double numero)
+        {
+            resultado = numero;
+        }
+    }
+}
---
+++ TP06-GomezRodrigo/calculadora/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+            int operacion, end = 1;
+
+            Console.WriteLine("Ingrese un valor para inicializar la calculadora: ");
+            valor=Convert.ToDouble(Console.ReadLine());
+            var nuevaCalculadora = new Calculadora(valor);
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese la operación que desea realizar [0]:SUMA - [1]:RESTA - [2]:MULTIPLICACIÓN - [3]:DIVISIÓN");
+                operacion = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el numero a operar: ");
+                valor = Convert.ToDouble(Console.ReadLine());
+
+                switch (operacion)
+                {
+                    case 0:
+                        nuevaCalculadora.Sumar(valor);
+                        break;
+
+                    case 1:
+                        nuevaCalculadora.Restar(valor);
+                        break;
+
+                    case 2:
+                        nuevaCalculadora.Multiplicar(valor);
+                        break;
+
+                    case 3:
+                        nuevaCalculadora.Dividir(valor);
+                        break;
+                }
+                if (!(operacion == 3 && valor == 0))
+                {
+                    Console.WriteLine($"El valor guardado en la calculadora es: {nuevaCalculadora.resultado}");
+                }
+                Console.WriteLine("Desea continuar usando la calculadora? [0]:NO - [1]:SI");
+                end = Convert.ToInt32(Console.ReadLine());
+            } while (end == 1);
+        }
+    }
+}
