2b3225ba3d0d11c79542aeae2d3aa23161e9f8aa
Roderick <iroderick@outlook.es>
2022-06-18 12:15:48 -0300

    tp completo

---
+++ TP06-GomezRodrigo/personal/Empleado.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personal
+{
+    public enum Cargos
+    {
+        Auxiliar,
+        Administrativo,
+        Ingeniero,
+        Especialista,
+        Investigador
+    }
+    internal class Empleado
+    {
+        private const int aniosDeAportes = 20;
+        internal string nombre { get; set; }
+        internal string apellido { get; set; }
+        internal DateTime fechaNacimiento { get; set; }
+        internal DateTime fechaIngreso { get; set; }
+        internal char estadoCivil { get; set; }
+        internal char genero { get; set; }
+        internal double sueldo { get; set; }
+        internal Cargos cargo { get; set; }
+        private int contarAnios(DateTime fecha)
+        {
+            int cantidad = 0;
+            if (DateTime.Today.DayOfYear < fecha.DayOfYear)
+            {
+                cantidad = DateTime.Today.Year - fecha.Year - 1;
+            }
+            else
+            {
+                cantidad = DateTime.Today.Year - fecha.Year;
+            }
+            return cantidad;
+        }
+        internal int Antiguedad (DateTime fecha)
+        {
+            int cantidad = 0;
+            cantidad = contarAnios(fecha);
+            return cantidad;
+        }
+        internal int Edad (DateTime fecha)
+        {
+            int cantidad = 0;
+            cantidad = contarAnios(fecha);
+            return cantidad;
+        }
+        internal int jubilacion (int antiguedad, int edad)
+        {
+            int edadControl, aniosRestantes = 0;
+            if (genero == 'm')
+            {
+                edadControl = 65;
+            }
+            else
+            {
+                edadControl = 60;
+            }
+            if (antiguedad >= 20)
+            {
+                if (edad >= edadControl)
+                {
+                    aniosRestantes = 0;
+                }
+                else
+                {
+                    aniosRestantes = edadControl - edad;
+                }
+            }
+            else
+            {
+                aniosRestantes = aniosDeAportes - antiguedad;
+            }
+            return aniosRestantes;
+        }
+        internal double Salario (int antiguedad)
+        {
+            double adicional = 0;
+            if (antiguedad < 20)
+            {
+                adicional += sueldo * (antiguedad / 100);
+            }
+            else
+            {
+                adicional += sueldo * .25;
+            }
+            if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+            {
+                adicional += adicional * .5;
+            }
+            if (estadoCivil == 'c')
+            {
+                adicional += 15000;
+            }
+            return sueldo + adicional;
+        }
+        internal Empleado()
+        {
+            fechaIngreso = DateTime.Now;
+            sueldo = new Random().Next(25000, 40000);
+            cargo = (Cargos)new Random().Next(5);
+        }
+        internal Empleado(string Nombre, string Apellido, string FechaNac, char EstadoCivil, char Genero)
+        {
+            nombre = Nombre;
+            apellido = Apellido;
+            fechaNacimiento = Convert.ToDateTime(FechaNac);
+            estadoCivil = EstadoCivil;
+            genero = Genero;
+        }
+    }
+}
---
+++ TP06-GomezRodrigo/personal/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace personal
+{
+    internal class Program
+    {
+        static void cargar (Empleado empleado)
+        {
+            string nombre, apellido, fechaNacimiento;
+            char estadoCivil, genero;
+
+            Console.WriteLine("Nombre: ");
+            empleado.nombre= Console.ReadLine();
+            Console.WriteLine("Apellido: ");
+            empleado.apellido = Console.ReadLine();
+            Console.WriteLine("Fecha de nacimiento dd/mm/aaaa: ");
+            empleado.fechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Estado civil [c]:casado [s]:soltero [v]:viudo: ");
+            empleado.estadoCivil = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Genero [m]:masculino [f]:femenino: ");
+            empleado.genero = Convert.ToChar(Console.ReadLine());
+        }
+        static void mostrar (Empleado empleado)
+        {
+            int antiguedad = 0, edad = 0, jubilacion = 0;
+            antiguedad = empleado.Antiguedad(empleado.fechaIngreso);
+            edad = empleado.Edad(DateTime.Now);
+            jubilacion = empleado.jubilacion(antiguedad, edad);
+            Console.WriteLine($"Nombre: {empleado.nombre}");
+            Console.WriteLine($"Apellido: {empleado.apellido}");
+            Console.WriteLine($"Fecha de nacimiento: {empleado.fechaNacimiento}");
+            Console.WriteLine($"Fecha de ingreso: {empleado.fechaIngreso}");
+            Console.WriteLine($"Estado Civil: {empleado.estadoCivil}");
+            Console.WriteLine($"Genero: {empleado.genero}");
+            Console.WriteLine($"Sueldo total: {empleado.Salario(antiguedad)}");
+            if (jubilacion == 0)
+            {
+                Console.WriteLine("El empleado ya puede jubilarse.");
+            }
+            else
+            {
+                Console.WriteLine($"A este empleado le faltan {jubilacion} años para jubilarse.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos a cargar 3 empleados.");
+            Console.WriteLine("Ingrese los datos del primer empleado: ");
+            var empleado1 = new Empleado();
+            cargar(empleado1);
+            //Console.WriteLine("Ingrese los datos del segundo empleado: ");
+            //var empleado2 = new Empleado();
+            //cargar (empleado2);j
+            //Console.WriteLine("Ingrese los datos del tercer empleado: ");
+            //var empleado3 = new Empleado();
+            //cargar(empleado3);
+            Console.WriteLine("Ahora vamos a mostrar los datos cargados de los 3 empleados");
+            Console.WriteLine("Empleado 1: ");
+            mostrar(empleado1);
+            //Console.WriteLine("Empleado 2: ");
+            //mostrar (empleado2);
+            //Console.WriteLine("Empleado 3: ");
+            //mostrar(empleado3);
+        }
+    }
+}
